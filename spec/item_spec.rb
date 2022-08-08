f4bde659cf1ce9c9b82b845ef1b794d2092afff3
Kevin Ta <kvnta@yahoo.com>
2022-08-08 09:09:59 -0600

    Complete tests for item class: iteration 1

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,11 +7,19 @@ RSpec.describe Item do
   end
 
 
-  describe 'iteration 1' do 
+  describe 'iteration 1 :item' do 
 
     it 'exists' do 
       expect(@item1).to be_a(Item)
       expect(@item2).to be_a(Item)
+    end
+
+    it 'has attributes' do 
+      expect(@item1.name).to eq("Peach")
+      expect(@item2.name).to eq("Tomato")
+
+      expect(@item1.price).to eq(0.75) #Eliminate / delete "$" from price
+      expect(@item2.price).to eq(0.5)
     end
   end
 end
