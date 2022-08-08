525d9e62eaca7bd67d321ec3551e852e7bc71f93
Kevin Ta <kvnta@yahoo.com>
2022-08-08 09:34:05 -0600

    Test: complete iteration 1 tests for vendor

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -23,11 +23,18 @@ RSpec.describe Vendor do
     end
 
     it 'can check inventory stock as well as stock an item and amount' do 
-      expect(@vendor.check_stock(@item1)).to eq(0)
 
+      expect(@vendor.check_stock(@item1)).to eq(0)
       @vendor.stock(@item1, 30)
 
       expect(@vendor.inventory).to eq({@item1 => 30})
+      expect(@vendor.check_stock(@item1)).to eq(30)
+
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(@item1)).to eq(55)
+
+      @vendor.stock(@item2, 12)
+      expect(@food_truck.inventory).to eq({@item => 55, @item2 => 12})
     end
 
   end
